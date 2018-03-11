9dac91835b3a20a13fdae9feda3e45da73ba9444
antony1803 <37068513+antony1803@users.noreply.github.com>
2018-03-12 02:57:36 +0300

    Update4
    вторая попытка для options

--- src/drawer/Options.java
+++ src/drawer/Options.java
@@ -10,8 +10,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Options extends JPanel {
+    static JRadioButton dr;
+    static JRadioButton mov;
     public Options(){
         super();
+        dr = new JRadioButton("Draw");
+        mov = new JRadioButton("Move");
+        ButtonGroup whatToDo = new ButtonGroup();
+        whatToDo.add(dr);
+        whatToDo.add(mov);
+        dr.setSelected(true);
+        add(dr);
+        add(mov);
         //Window.getFigures().add(new Diamond(new Point(0, 0), new Point(8,0), new Point(7,1)));
         //System.out.println(new Rectangle(new Point(50, 50), new Point(100, 100)).getNumberOfClicks());
         //System.out.println(new Diamond(new Point(50, 50), new Point(200, 200), new Point(150, 200)).getNumberOfClicks());
